3adf62dbbcc50aa770f4c869d00bfef4db743f8f
torrseba <sebastian.antonio.torres@gmail.com>
2023-02-20 11:00:41 -0500

    maybe form works

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 alert('test')
 //document.body.innerHTML = '<h1>lol<h1>';
+let input1 =document.getElementsByName('xcoordinate');
+console.log(input1);
+
+let astro = document.getElementById("astro");
+astro.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let xcord = document.getElementByname("xcoordinate");
+    let ycord = document.getElementByname("ycoordinate");
+  
+    if (xcord.value == "" || ycord.value == "") {
+        alert("Ensure you input a value in both fields!");
+      } else {
+        // perform operation with form input
+        alert("This form has been successfully submitted!");
+        console.log(
+          `This form has a xcord of ${xcord.value} and password of ${ycord.value}`
+        );
+        xcord.value = "";
+        ycord.value = "";
+      }
+    // handle submit
+  });
