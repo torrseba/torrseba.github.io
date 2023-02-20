8086f99eff30da7222be3f1e021d9dd314b01c49
torrseba <113491671+torrseba@users.noreply.github.com>
2023-02-19 23:06:27 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Sebastian Torres</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<section id = "image1">
+<p>this is HTML</p>	
+<?php
+    include (runner.php)
+    ?>
+</section>
+
+
+</body>
+
+</html>
---
+++ runner.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Amongus and minecraft"
+    
+    ?>
