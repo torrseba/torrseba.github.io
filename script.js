a23889ffcdebaa03659f1526afa181b1a2eb14bf
torrseba <sebastian.antonio.torres@gmail.com>
2023-02-22 12:43:54 -0500

    Display Image Added

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ astro.addEventListener("submit", (e) => {
     document.write(str);
     console.log(jsonObj.data.imageUrl);
     //document.write(str.data.imgUrl);
-    displayImage(jsonObj.data.imageUrl);
+    displayImage(jsonObj.data.imageUrl, 300, 300);
     /////////displayImage(str.data, 300, 300);
 })
 .catch(function(error){
